f4cc8b3169782cd6a3e0304cd4045254b320046e
María Agustina Nahas <maria.nahas@globant.com>
2016-10-26 17:54:10 -0300

    Intento fallido de manejo de errores

--- 01-JS-DOM-APIs/script.js
+++ 01-JS-DOM-APIs/script.js
@@ -1,12 +1,18 @@
 function getJoke(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var json = JSON.parse(this.responseText);
-			document.getElementById("hidden").innerHTML = json.value.joke;
+		try {
+			if (this.readyState==4 && this.status==200) {   
+				var json = JSON.parse(this.responseText);
+				document.getElementById("hidden").innerHTML = json.value.joke;
 			}
+		}
+		catch(e) {
+			alert("Error" + e);  
+		}
 	};
 	xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
 	xhttp.send();
 }
+
 
