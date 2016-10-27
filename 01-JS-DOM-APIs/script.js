b62e6f077439c8e30b710cc441a30db80a925e78
María Agustina Nahas <maria.nahas@globant.com>
2016-10-27 14:33:37 -0300

    Service manejado por AJAX, error mostrado.

--- 01-JS-DOM-APIs/script.js
+++ 01-JS-DOM-APIs/script.js
@@ -1,18 +1,37 @@
+$(document).ready(function(){
+	$("#hidden").fadeIn("2000");
+});
+
 function getJoke(){
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		try {
-			if (this.readyState==4 && this.status==200) {   
-				var json = JSON.parse(this.responseText);
-				document.getElementById("hidden").innerHTML = json.value.joke;
-			}
+	var configJoke = {
+		url: 'http://api.icndb.com/jokes/random',
+		dataType: 'json',
+		success: function( resp ) {
+			$('#hidden').html( resp.value.joke );
+		},
+		error: function( req, status, err ) {
+			document.getElementById("hidden").style.color = '#ff0000';
 		}
-		catch(e) {
-			alert("Error" + e);  
-		}
-	};
-	xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
-	xhttp.send();
+	}
+	// Initiate the request!
+	$.ajax(configJoke);
 }
+
+// function getJoke(){
+// 	var xhttp = new XMLHttpRequest();
+// 	xhttp.onreadystatechange = function() {
+// 		try {
+// 			if (this.readyState==4 && this.status==200) {   
+// 				var json = JSON.parse(this.responseText);
+// 				document.getElementById("hidden").innerHTML = json.value.joke;
+// 			}
+// 		}
+// 		catch(e) {
+// 			alert("Error" + e);  
+// 		}
+// 	};
+// 	xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
+// 	xhttp.send();
+// }
 
 
