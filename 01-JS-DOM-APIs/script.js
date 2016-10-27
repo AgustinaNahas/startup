1ea76199f073d168d400605978e6103e397916c2
María Agustina Nahas <maria.nahas@globant.com>
2016-10-27 17:09:46 -0300

    Finished point 9

--- 01-JS-DOM-APIs/script.js
+++ 01-JS-DOM-APIs/script.js
@@ -2,36 +2,73 @@ $(document).ready(function(){
 	$("#hidden").fadeIn("2000");
 });
 
-function getJoke(){
-	var configJoke = {
-		url: 'http://api.icndb.com/jokes/random',
-		dataType: 'json',
-		success: function( resp ) {
-			$('#hidden').html( resp.value.joke );
-		},
-		error: function( req, status, err ) {
-			document.getElementById("hidden").style.color = '#ff0000';
-		}
-	}
-	// Initiate the request!
-	$.ajax(configJoke);
+var ajax = function (options) {
+	return new Promise(function (resolve, reject) {
+  	var request = new XMLHttpRequest();  
+    var async = true; // siempre debe ser asíncrono el llamado
+    request.onload = resolve;
+    request.onerror = reject;
+    
+    if(options.method === 'GET' && options.data) {
+			    options.url += '?' + options.data;
+    }
+    
+    request.open(options.method, options.url, async);
+    
+    if(options.contentType) {
+    	request.setRequestHeader('Content-type', options.contentType);
+    }
+    
+    if(options.method !== 'GET' && options.data) {
+    	request.send(options.data);
+    } else {
+    	request.send();
+    }
+  });
 }
 
-// function getJoke(){
-// 	var xhttp = new XMLHttpRequest();
-// 	xhttp.onreadystatechange = function() {
-// 		try {
-// 			if (this.readyState==4 && this.status==200) {   
-// 				var json = JSON.parse(this.responseText);
-// 				document.getElementById("hidden").innerHTML = json.value.joke;
-// 			}
-// 		}
-// 		catch(e) {
-// 			alert("Error" + e);  
-// 		}
-// 	};
-// 	xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
-// 	xhttp.send();
-// }
+function getResponse(config_obj) {
+	ajax(config_obj).then(function (response) {
+	  if(response.target.response !== '') {
+	  	config_obj.success(response);
+	  }
+	}, function (response) {
+		config_obj.error(response);
+	});
+}
 
+var config_joke = {
+	url: 'http://api.icndb.com/jokes/random',
+	dataType: 'json',
+	method: 'GET', 
+	success: function (response) {
+		$('#display_text').html(JSON.parse(response.target.response).value.joke);
+	},
+	error: function (resp) {
+		document.getElementById("display_text").style.color = '#ff0000';
+	}
+}
 
+var config_repo = {
+	url: 'https://api.github.com/search/repositories',
+	dataType: 'json',
+	method: 'GET', 
+	data: "q=Javascript",
+	success: function (response) {
+		$('#display_text').html("");
+		var json_repositories = JSON.parse(response.target.response).items;
+		json_repositories.forEach( function( item ) {
+	    	add_to_list(item.full_name);
+	    });
+	},
+	error: function (resp) {
+		document.getElementById("hidden").style.color = '#ff0000';
+	}
+}
+
+function add_to_list (full_name) {
+	var new_repo = document.createElement("li");
+	var repo_name = document.createTextNode(full_name);
+	new_repo.appendChild(repo_name);
+	document.getElementById("repository_list").appendChild(new_repo);
+}
