2578c818d3c69a4c75f32b41a7a2c7bf0b7d07e7
María Agustina Nahas <maria.nahas@globant.com>
2016-11-01 17:03:37 -0300

    Hasta el punto 7, falta punto 5

--- 02-oop-inheritance/script.js
+++ 02-oop-inheritance/script.js
@@ -42,4 +42,13 @@ function Actor(name, age){
 	}
 }
 
- 
+function Social(title, year, duration){
+	var m = Object.create(new Movie(title, year, duration));
+	m.like = function(friend) {
+		console.log(friend + " likes " + this.title);
+	};
+	m.share = function(friend) {
+		console.log(friend + " shared " + this.title);
+	}
+	return m;
+}
