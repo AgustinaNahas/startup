115696b6cf0de5a4ef47faeb6f4313cb2c36220f
María Agustina Nahas <maria.nahas@globant.com>
2016-11-01 17:20:23 -0300

    Puntos 5 y 8 terminados

--- 02-oop-inheritance/script.js
+++ 02-oop-inheritance/script.js
@@ -1,4 +1,5 @@
 function EventEmmiter(){
+	var eventHandlers = [];
 	return {
 		on: function(name, callback){
 			eventHandlers.push({name, callback});
@@ -14,18 +15,39 @@ function EventEmmiter(){
 	};
 }
 
-var movie1 = Movie('What a wonderful life', 1946, '2h 10min');
-var movie2 = Movie('The Shining', 1980, '2h 26min');
-var movie3 = Movie('Amadeus', 1984, '2h 40min');
-var movie4 = Movie('Back to the Future', 1985, '1h 56min');
-var movie5 = Movie('The Fountain', 2006, '1h 36min');
-
 function Movie(title, year, duration){
-	var m = Object.create(new EventEmmiter());
-	m.title = title;
-	m.year = year;
-	m.duration = duration;
-	return m;
+	var sup = Object.create(new EventEmmiter());
+
+	function on(eventName, callback) {
+		sup.on(eventName, function() {
+			callback(eventName + " has been emitted");
+		})
+	}
+
+	function emit(eventName) {
+		sup.emit(eventName);
+	}
+
+	function off(eventName) {
+		sup.off(eventName);
+	}
+
+	return {
+		title: title,
+		year: year,
+		duration: duration,
+		on: on,
+		emit: emit, 
+		off: off,
+		add_cast: function(new_cast){
+			if(Array.isArray(new_cast)){
+				m.cast = m.cast.concat(new_cast)
+			} else {
+				m.cast.push(new_cast);
+			}
+		}
+
+	};
 }
 
 function Logger(){
@@ -52,3 +74,9 @@ function Social(title, year, duration){
 	}
 	return m;
 }
+
+var movie1 = Movie('What a wonderful life', 1946, '2h 10min');
+var movie2 = Movie('The Shining', 1980, '2h 26min');
+var movie3 = Movie('Amadeus', 1984, '2h 40min');
+var movie4 = Movie('Back to the Future', 1985, '1h 56min');
+var movie5 = Movie('The Fountain', 2006, '1h 36min');
