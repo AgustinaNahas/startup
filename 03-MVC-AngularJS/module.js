9308d5506db2fc5cedf0d1f4b6b939797e439f5c
María Agustina Nahas <maria.nahas@globant.com>
2016-11-03 18:06:27 -0300

    Terminado el punto 3, de manera no muy decorosa

--- 03-MVC-AngularJS/module.js
+++ 03-MVC-AngularJS/module.js
@@ -22,13 +22,86 @@ myApp.service('Storage_Serv', function(Storage){
 	}
 });
  
- myApp.controller('MainCtrl', function($scope, Storage_Serv) {
+myApp.controller('MainCtrl', function($scope, Storage_Serv) {
 
- 	// Storage_Serv.post(movie, 'movies');
+	//Storage_Serv.post(movie, 'movies');
 
-    $scope.movie_list = Storage_Serv.get('movies');
+	$scope.movie_list = Storage_Serv.get('movies');
 
- });
+	$scope.hide_details = true;
+	$scope.selected_movie;
+
+	$scope.show_details = function(movie) {
+		$scope.hide_details = $scope.change_visibility(movie);
+	};
+
+	$scope.get_selected_movie = function() {
+		return $scope.selected_movie;
+	};
+
+	$scope.change_visibility = function(movie) {
+		
+		if($scope.selected_movie == movie && !$scope.hide_details) 
+			$scope.hide_details = true;
+		else 
+		{
+			$scope.selected_movie = movie;
+			$scope.hide_details = false;
+		}
+		
+		return $scope.hide_details;
+	};
+
+	$scope.add_movie = function(){
+		if (document.getElementById('in_movie_title').value != '' && document.getElementById('in_movie_year').value != '' && document.getElementById('in_movie_duration').value != '') {
+			var title = document.getElementById('in_movie_title').value;
+			var year = parseInt(document.getElementById('in_movie_year').value);//$('id_movie_year');
+			var duration = document.getElementById('in_movie_duration').value;
+		}
+
+		var new_movie = new Movie(title, year, duration);
+		
+		var movie_list = Storage_Serv.get('movies');
+		
+		movie_list.push(new_movie);
+		
+		Storage_Serv.post(movie_list, 'movies');
+		
+		$scope.movie_list = Storage_Serv.get('movies');
+
+		document.getElementById("add_button").className = "btn btn-success";
+		document.getElementById("add_button").innerHTML = "Agregar";
+	};
+
+	$scope.delete_movie = function(){
+		var title = document.getElementById('in_movie_title').value;
+		
+		var movie_list = Storage_Serv.get('movies');
+		var index = movie_list.findIndex(mov => mov.title == title);
+		if (index != undefined) movie_list.splice(index, 1);
+		Storage_Serv.post(movie_list, 'movies');
+		
+		$scope.movie_list = Storage_Serv.get('movies');
+	}
+
+	$scope.edit_movie = function(){
+		document.getElementById('in_movie_title').value = $scope.selected_movie.title;
+		document.getElementById('in_movie_year').value = $scope.selected_movie.year;
+		document.getElementById('in_movie_duration').value = $scope.selected_movie.duration;
+
+		var title = $scope.selected_movie.title;
+		
+		var movie_list = Storage_Serv.get('movies');
+		var index = movie_list.findIndex(mov => mov.title == title);
+		if (index != undefined) movie_list.splice(index, 1);
+		Storage_Serv.post(movie_list, 'movies');
+		
+		$scope.movie_list = Storage_Serv.get('movies');
+
+		document.getElementById("add_button").className = "btn btn-primary";
+		document.getElementById("add_button").innerHTML = "Guardar";
+	}
+});
 
 // var movie = [
 // 	new Movie('What a wonderful life', 1946, '2h 10min'), 
@@ -37,3 +110,4 @@ myApp.service('Storage_Serv', function(Storage){
 // 	new Movie('Back to the Future', 1985, '1h 56min'), 
 // 	new Movie('The Fountain', 2006, '1h 36min')
 // ];
+
